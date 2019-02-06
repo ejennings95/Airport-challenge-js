e32c261d04e6508e8214c5ad4356a3860e709ab1
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-06 17:28:21 +0000

    weather generator added and tested

--- spec/WeatherSpec.js
+++ spec/WeatherSpec.js
@@ -1,1 +1,26 @@
 var Weather = require('../src/Weather.js')
+
+describe("Weather", function() {
+  var weather;
+
+  beforeEach(function() {
+    weather = new Weather();
+  });
+
+  it("Should return a number between 1-10", function(){
+    expect(weather.randomGenerator()).toBeGreaterThan(0);
+    expect(weather.randomGenerator()).toBeLessThan(11);
+  });
+
+  it("Should return false to sunnyWeather", function(){
+    spyOn(Math, 'random').and.returnValue(0.9);
+    weather.check()
+    expect(weather.status()).toEqual(false);
+  });
+
+  it("Should return true to sunnyWeather", function(){
+    spyOn(Math, 'random').and.returnValue(0.2);
+    weather.check()
+    expect(weather.status()).toEqual(true);
+  });
+});
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,21 @@
 function Weather() {
+  this._sunnyWeather = true;
+};
 
+Weather.prototype.status = function(){
+  return this._sunnyWeather;
+};
+
+Weather.prototype.randomGenerator = function() {
+  return Math.ceil(Math.random() * 10)
+};
+
+Weather.prototype.check = function() {
+  if (this.randomGenerator() > 8) {
+    this._sunnyWeather = false
+  } else {
+    this._sunnyWeather = true
+  };
 };
 
 module.exports = Weather
